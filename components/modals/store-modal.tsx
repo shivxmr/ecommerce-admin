ea932db465e275af5a5015a60b2a75e0ad62c454
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-08-02 00:45:52 +0530

    added form components (admin)

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,47 +1,48 @@
 "use client";
 
-// import * as z from "zod"
-// // import axios from "axios";
-// // import { zodResolver } from "@hookform/resolvers/zod"
-// // import { useForm } from "react-hook-form";
-// // import { toast } from "react-hot-toast";
-// import { useRouter } from "next/navigation";
-// import { useState } from "react";
+import * as z from "zod"
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 import { Modal } from "@/components/ui/modal";
-// import { Input } from "@/components/ui/input";
-// import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { useStoreModal } from "@/hooks/use-store-modal";
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
-// const formSchema = z.object({
-//   name: z.string().min(1),
-// });
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
-//   const router = useRouter();
+  const router = useRouter();
 
-//   const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-//   const form = useForm<z.infer<typeof formSchema>>({
-//     resolver: zodResolver(formSchema),
-//     defaultValues: {
-//       name: "",
-//     },
-//   });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
-//   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-//     try {
-//       setLoading(true);
-//       const response = await axios.post('/api/stores', values);
-//       window.location.assign(`/${response.data.id}`);
-//     } catch (error) {
-//       toast.error('Something went wrong');
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // try {
+    //   setLoading(true);
+    //   const response = await axios.post('/api/stores', values);
+    //   window.location.assign(`/${response.data.id}`);
+    // } catch (error) {
+    //   toast.error('Something went wrong');
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
 
   return (
     <Modal
@@ -50,7 +51,7 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen} 
       onClose={storeModal.onClose}
     >
-      {/* <div>
+      <div>
         <div className="space-y-4 py-2 pb-4">
           <div className="space-y-2">
             <Form {...form}>
@@ -78,7 +79,7 @@ export const StoreModal = () => {
             </Form>
           </div>
         </div>
-      </div> */}
+      </div>
       Future Create Store Form
     </Modal>
   );
