96b272c6f87b134d0d9266bcc3dfa511038f7dd7
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-08-02 19:31:56 +0530

    minor change

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,13 @@
 import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
+import { MainNav } from "@/components/main-nav";
 // import { ThemeToggle } from "@/components/theme-toggle";
 import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
- const { userId } = auth();
+  const { userId } = auth();
 
   if (!userId) {
     redirect('/sign-in');
@@ -19,17 +19,18 @@ const Navbar = async () => {
     }
   });
 
-    return ( 
-        <div className="border-b">
-            <div className="flex h-16 items-center px-4">
-            <StoreSwitcher items={stores}/>
-                <MainNav className="mx-6"/>
-                <div className="ml-auto flex items-center space-x-4">
-                    <UserButton afterSignOutUrl="/"/>
-                </div>
-            </div>
+  return ( 
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          {/* <ThemeToggle /> */}
+          <UserButton afterSignOutUrl="/" />
         </div>
-     );
-}
+      </div>
+    </div>
+  );
+};
  
 export default Navbar;
